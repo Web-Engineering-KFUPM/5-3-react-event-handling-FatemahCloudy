e60ecd8b31eaaa2a2ed4523cf701208a8ad9a810
Fatemah Almarhoon <fatemahrauf15@gmail.com>
2025-10-06 11:46:24 +0300

    Task 1 done

--- 5-3-react-event-handling/src/components/TaskApp.jsx
+++ 5-3-react-event-handling/src/components/TaskApp.jsx
@@ -2,7 +2,9 @@ import React, { useState } from "react";
 import TaskList from "./TaskList";
 
 export default function TaskApp() {
-  
+    
+    const [text, setText] = useState("");
+
   const handleSubmit = () => {
    
   };
@@ -18,34 +20,35 @@ export default function TaskApp() {
   };
 
   return (
-    <section className="card">
-      {/*Controlled Input */}
-      <div className="inputRow">
-        <input
-          type="text"
-          placeholder="Type a task..."
-          className="input"
-          // TODO: value={text}
-          // TODO: onChange={(e) => setText(e.target.value)}
-          onKeyDown={(e) => {
-            if (e.key === "Enter") handleSubmit();
-          }}
-        />
-        <button className="btn btn--primary" onClick={handleSubmit}>
-          Submit
-        </button>
-      </div>
+      <section className="card">
+          {/*Controlled Input */}
+          <div className="inputRow">
+              <input
+                  type="text"
+                  placeholder="Type a task..."
+                  className="input"
+                  value={text}
+                  onChange={(e) => setText(e.target.value)}
+                  onKeyDown={(e) => {
+                      if (e.key === "Enter") handleSubmit();
+                  }}
+              />
+              <button className="btn btn--primary" onClick={handleSubmit}>
+                  Submit
+              </button>
+          </div>
 
-      {/*Render Task List and Enable Delete */}
-      {/*Pass tasks and onDelete */}
-      <TaskList /* tasks={tasks} onDelete={handleDelete} */ />
+          <p>{text}</p>
+          {/*Render Task List and Enable Delete */}
+          {/*Pass tasks and onDelete */}
+          <TaskList /* tasks={tasks} onDelete={handleDelete} */ />
 
-      {/*Clear All */}
-      <div className="footerRow">
-        <button className="btn btn--ghost" onClick={handleClearAll}>
-          Clear All
-        </button>
-      </div>
-    </section>
+          {/*Clear All */}
+          <div className="footerRow">
+              <button className="btn btn--ghost" onClick={handleClearAll}>
+                  Clear All
+              </button>
+          </div>
+      </section>
   );
 }
